a7b17a88a41431d101649ce6ac7e1557ed30db98
NeithanVeliz3C <isidoraveliz@liceovvh.cl>
2025-04-02 08:56:40 -0300

    ok

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-function numPares() {
-    let numInicio = parseInt(prompt("Ingresa el número inicial:"));
-    let numFinal = parseInt(prompt("Ingresa el número final:"));
-    let array = [];
-    for (let i = numInicio; i <= numFinal; i++) {
-        if (i % 2 === 0) {
-            array.push(i);
-        }
-    }
-    alert("Los números pares entre" + " " + numInicio + " " + "y" + " " + numFinal + " " + "son:" + " " + array.join(" - "));
-}
-
 function generarPares() {
     let pares = [];
     for (let i = 1; i <= 100; i++) {
       pares.push(i * 2);
     }
     return pares;
-  }
-  
-  const primerosCienPares = generarPares();
-  console.log(primerosCienPares);
+}
+const primerosCienPares = generarPares();
+console.log(primerosCienPares);
